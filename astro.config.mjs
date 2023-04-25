d60ed4fe9aaabffb81be8c9523124bd9fcaec9ca
Tom Rose <tom@slurve.com>
2023-04-25 13:19:43 -0400

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+// astro.config.mjs
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
